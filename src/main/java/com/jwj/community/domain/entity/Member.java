c0b7fccc7904f6f9a3d6a9c2523098658298a0c2
w32807 <w32807@nate.com>
2022-09-11 12:34:32 +0900

    1. 이메일 필드에 unique 제약조건 추가

--- src/main/java/com/jwj/community/domain/entity/Member.java
+++ src/main/java/com/jwj/community/domain/entity/Member.java
@@ -28,6 +28,7 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String email;
     private String password;
     private String nickname;
