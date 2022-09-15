f144ba0ec3bbd5b47e9394e8a2db7796c570f384
w32807 <w32807@nate.com>
2022-09-15 23:14:05 +0900

    1. JwtTokenUtil 테스트 작성

--- src/test/java/com/jwj/community/config/security/utils/JwtTokenUtilTest.java
+++ src/test/java/com/jwj/community/config/security/utils/JwtTokenUtilTest.java
@@ -1,64 +1,111 @@
 package com.jwj.community.config.security.utils;
 
-import com.jwj.community.web.login.request.JwtRequest;
+import com.jwj.community.domain.common.enums.Roles;
+import com.jwj.community.domain.entity.Member;
+import com.jwj.community.web.login.jwt.JwtToken;
 import com.jwj.community.web.login.request.MemberSaveRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
+import java.util.Set;
 
+import static com.jwj.community.domain.common.enums.Roles.ROLE_ADMIN;
+import static com.jwj.community.domain.common.enums.Roles.ROLE_MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JwtTokenUtilTest {
 
     private JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
 
-    MemberSaveRequest request;
+    Member member;
 
     @BeforeEach
     void setup(){
-        request = MemberSaveRequest.builder()
+        member = MemberSaveRequest.builder()
                 .email("admin@google.com")
                 .password("1234")
                 .nickname("닉네임")
-                .build();
+                .build()
+                .toEntity();
     }
 
     @Test
-    @DisplayName("토큰 생성 성공")
+    @DisplayName("회원정보가 유효할 때 access 토큰얻기")
     void test1() throws Exception{
-        // given
-        // when
-        JwtRequest jwtRequest = jwtTokenUtil.generateToken(request.toEntity());
+        JwtToken token = jwtTokenUtil.generateToken(member);
 
-        // then
-        assertThat(jwtRequest.getToken()).isNotNull();
+        assertThat(token.getAccessToken()).isNotNull();
     }
 
     @Test
-    @DisplayName("토큰으로부터 회원 이메일 얻기")
+    @DisplayName("회원정보가 유효할 때 refresh 토큰얻기")
     void test2() throws Exception{
-        // given
-        // when
-        JwtRequest jwtRequest = jwtTokenUtil.generateToken(request.toEntity());
-        String usernameFromToken = jwtTokenUtil.getUsernameFromToken(jwtRequest.getToken());
+        JwtToken token = jwtTokenUtil.generateToken(member);
 
-        // then
+        assertThat(token.getRefreshToken()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("회원정보가 유효하지 않을 때 access 토큰은 null")
+    void test3() throws Exception{
+        MemberSaveRequest memberSaveRequest = MemberSaveRequest.builder().build();
+
+        JwtToken token = jwtTokenUtil.generateToken(memberSaveRequest.toEntity());
+        assertThat(token.getAccessToken()).isNull();
+    }
+
+    @Test
+    @DisplayName("회원정보가 유효하지 않을 때 refresh 토큰은 null")
+    void test4() throws Exception{
+        MemberSaveRequest memberSaveRequest = MemberSaveRequest.builder().build();
+
+        JwtToken token = jwtTokenUtil.generateToken(memberSaveRequest.toEntity());
+        assertThat(token.getRefreshToken()).isNull();
+    }
+
+    @Test
+    @DisplayName("회원이 null일 때 access 토큰은 null")
+    void test8() throws Exception{
+        JwtToken token = jwtTokenUtil.generateToken(null);
+        assertThat(token.getAccessToken()).isNull();
+    }
+
+    @Test
+    @DisplayName("회원이 null일 때 refresh 토큰은 null")
+    void test9() throws Exception{
+        JwtToken token = jwtTokenUtil.generateToken(null);
+        assertThat(token.getRefreshToken()).isNull();
+    }
+
+    @Test
+    @DisplayName("토큰에서 username 조회")
+    void test5() throws Exception{
+        JwtToken token = jwtTokenUtil.generateToken(member);
+        String usernameFromToken = jwtTokenUtil.getUsernameFromToken(token.getAccessToken());
+
         assertThat(usernameFromToken).isEqualTo("admin@google.com");
     }
 
     @Test
-    @DisplayName("토큰으로부터 만료시간 얻기")
-    void test3() throws Exception{
-        // given
-        // when
-        JwtRequest jwtRequest = jwtTokenUtil.generateToken(request.toEntity());
-        Date expirationDateFromToken = jwtTokenUtil.getExpirationDateFromToken(jwtRequest.getToken());
+    @DisplayName("토큰에서 권한조회 조회")
+    void test6() throws Exception{
+        member.addRole(ROLE_ADMIN);
+        member.addRole(ROLE_MEMBER);
 
-        System.out.println("expirationDateFromToken = " + expirationDateFromToken);
-        // then
-        //assertThat(usernameFromToken).isEqualTo("admin@google.com");
+        JwtToken token = jwtTokenUtil.generateToken(member);
+        Set<Roles> rolesFromToken = jwtTokenUtil.getRolesFromToken(token.getAccessToken());
+        assertThat(rolesFromToken)
+                .hasSize(2)
+                .contains(ROLE_ADMIN, ROLE_MEMBER);
+    }
+
+    @Test
+    @DisplayName("토큰에서 권한이 없는데 권한 조회")
+    void test7() throws Exception{
+        JwtToken token = jwtTokenUtil.generateToken(member);
+        Set<Roles> rolesFromToken = jwtTokenUtil.getRolesFromToken(token.getAccessToken());
+        assertThat(rolesFromToken).isEmpty();
     }
 
 }
