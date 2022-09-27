5264e29a91d5497c61b9c1b6c4c8759732e529ac
w32807 <w32807@nate.com>
2022-09-27 23:14:05 +0900

    1. 스프링 시큐리티에서 cors 설정할 수 있도록 수정

--- src/main/java/com/jwj/community/config/security/config/JwtSecurityConfig.java
+++ src/main/java/com/jwj/community/config/security/config/JwtSecurityConfig.java
@@ -12,7 +12,6 @@ import com.jwj.community.domain.refreshToken.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -26,11 +25,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Order(0)
 @Configuration
 @RequiredArgsConstructor
 public class JwtSecurityConfig {
@@ -58,6 +59,8 @@ public class JwtSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.csrf().disable();
+
+        http.cors();
 
         http.addFilterBefore(jwtLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -99,5 +102,14 @@ public class JwtSecurityConfig {
     @Bean
     public AuthenticationFailureHandler jwtAuthenticationFailureHandler(){
         return new JwtAuthenticationFailureHandler();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://127.0.0.1:5173");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
