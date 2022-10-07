cce7713773c918a42719f0fb6e782c3cfe4e1ca4
w32807 <w32807@nate.com>
2022-10-07 23:29:49 +0900

    1. vuex store 사용을 위한 의존성 추가 2. store.ts 파일 추가

---
+++ front/src/store.ts
@@ -0,0 +1,29 @@
+import { createStore } from "vuex";
+import Member from "./store/models/member";
+
+export default createStore({
+    state: {
+        isAuthenticated: false,
+        member: new Member()
+    },
+    getters: {
+        // 위의 state를 매개변수로 받는다.
+        getMember(state) {
+            return state.member;
+        },
+        isAuthenticated(state) {
+            return state.isAuthenticated;
+        }
+    },
+    mutations: {
+        loginSuccess(state, member) {
+            state.isAuthenticated = true;
+            state.member = member;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+            state.member.email = null;
+            state.member.name = null;
+        },
+    }
+});
