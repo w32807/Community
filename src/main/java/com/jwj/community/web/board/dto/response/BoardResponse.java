968798695789fad3da8a0bc339578c65a2399f06
w32807 <w32807@nate.com>
2022-10-23 09:31:02 +0900

    1. 들여쓰기 수정

--- src/main/java/com/jwj/community/web/board/dto/response/BoardResponse.java
+++ src/main/java/com/jwj/community/web/board/dto/response/BoardResponse.java
@@ -2,7 +2,7 @@ package com.jwj.community.web.board.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jwj.community.domain.entity.Board;
-import com.jwj.community.web.member.dto.response.MemberEmailResponse;
+import com.jwj.community.web.member.dto.response.WriteMemberResponse;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,18 +15,18 @@ public class BoardResponse {
     private Long id;
     private String title;
     private String content;
-    private MemberEmailResponse member;
+    private WriteMemberResponse member;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime regDate;
 
     public static BoardResponse of(Board board){
         return BoardResponse.builder()
-            .id(board.getId())
-            .title(board.getTitle())
-            .content(board.getContent())
-            .regDate(board.getRegDate())
-            .regDate(board.getModDate())
-            .build();
+                .id(board.getId())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .regDate(board.getModDate())
+                .build();
     }
 }
