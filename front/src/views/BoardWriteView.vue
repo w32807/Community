dd9890b46308d079994ed799ca4ae84aa2c5eba1
w32807 <w32807@nate.com>
2022-10-26 23:02:00 +0900

    1. write 함수명 변경

--- front/src/views/BoardWriteView.vue
+++ front/src/views/BoardWriteView.vue
@@ -7,14 +7,14 @@ const title = ref("");
 const content = ref("");
 const router = useRouter();
 
-const write = function (){
+const writeBoard = function (){
     axios.post('/board/board',{
         title: title.value,
         content: content.value
     })
     .then(() => {
         // 글 작성 완료 후 화면 이동하는 방법
-      router.push({name: "home"});
+        router.push({name: "home"});
     })
     .catch(error => {
         console.log(error)
@@ -27,7 +27,7 @@ const write = function (){
     <div>
         <el-input class="mt-2" v-model="title" placeholder="제목을 입력 해 주세요"/>
         <el-input class="mt-2" v-model="content" type="textarea" rows="15"/>
-        <el-button class="mt-2" type="primary" @click="write()">작성완료</el-button>
+        <el-button class="mt-2" type="primary" @click="writeBoard()">작성완료</el-button>
     </div>
 </template>
 
